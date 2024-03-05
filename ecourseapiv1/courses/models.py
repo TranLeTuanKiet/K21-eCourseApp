3278ab5fe7aeb66fc310a62fedd2cd227a9330bc
Your Name <2151053033kiet@ou.edu.vn>
2024-03-05 16:37:23 +0700

    create

---
+++ ecourseapiv1/courses/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Course(BaseModel):
+    name = models.CharField(max_length=225)
+    description = RichTextField()
+    image = models.ImageField(upload_to='courses/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemBase(BaseModel):
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        abstract = True
+
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = models.ImageField(upload_to='lesson/%Y/%M/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.name
